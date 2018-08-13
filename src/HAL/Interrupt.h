928137dcdd9096541562e3505383cc82f435ca34
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2018-08-13 17:50:09 +0800

    Create Interrupt.h

---
+++ src/HAL/Interrupt.h
@@ -0,0 +1,19 @@
+/*
+ * Interrupt.h
+ *
+ *  Created on: 2014-4-30
+ *      Author: Administrator
+ */
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+extern void P2_IODect();
+extern void P21_Onclick();
+extern void P1_IODect();
+extern void P11_Onclick();
+extern void TA1_Dect(void);
+extern void USCI_A0_ISR(void);
+
+
+#endif /* INTERRUPT_H_ */
