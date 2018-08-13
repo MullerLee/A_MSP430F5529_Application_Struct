3cbe54c821ecdb44220f51286e06db7b35739503
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2018-08-13 17:28:57 +0800

    Create ADC_5529.h

---
+++ src/ADC_5529.h
@@ -0,0 +1,15 @@
+/*
+ * ADC_5529.h
+ *
+ *  Created on: 2018年7月15日
+ *      Author: Muller Lee (X.C.Lee)
+ */
+
+#ifndef SRC_ADC_5529_H_
+#define SRC_ADC_5529_H_
+
+
+void ADC_All_Init(void);
+
+
+#endif /* SRC_ADC_5529_H_ */
