0467738b58d867300819b011218aab8f59ef51a1
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2018-08-13 17:46:45 +0800

    Create TimerA1.h

---
+++ src/TimerA1.h
@@ -0,0 +1,14 @@
+/*
+ * TimerA1.h
+ *
+ *  Created on: 2018年7月14日
+ *      Author: Muller Lee (X.C.Lee)
+ */
+
+#ifndef SRC_TIMERA1_H_
+#define SRC_TIMERA1_H_
+
+void TimerA1_0_init(int period);
+void TimerA1_0_int_init(void);
+
+#endif /* SRC_TIMERA1_H_ */
