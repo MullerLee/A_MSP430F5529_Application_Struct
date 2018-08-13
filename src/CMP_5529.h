60471feb6bde2dade2001ed6c75b5e81fb7fc978
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2018-08-13 17:33:47 +0800

    Create CMP_5529.h

---
+++ src/CMP_5529.h
@@ -0,0 +1,16 @@
+/*
+ * CMP_5529.h
+ *
+ *  Created on: 2018年7月13日
+ *      Author: Muller Lee (X.C.Lee)
+ */
+
+#ifndef SRC_CMP_5529_H_
+#define SRC_CMP_5529_H_
+
+void CB0_Init(void);
+void CB1_Init(void);
+void CB2_Init(void);
+void CB3_Init(void);
+
+#endif /* SRC_CMP_5529_H_ */
